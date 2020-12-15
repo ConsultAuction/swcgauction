d89e8bff5c8e4b33eba710d2e34755c26ecf1ef6
Adam Lundberg <adamlundberg91@gmail.com>
2020-12-15 14:04:51 +0100

    Initial commit

---
+++ src/main/java/se/swcg/consultauction/ConsultauctionApplication.java
@@ -0,0 +1,13 @@
+package se.swcg.consultauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultauctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultauctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/swcg/consultauction/ConsultauctionApplicationTests.java
@@ -0,0 +1,13 @@
+package se.swcg.consultauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultauctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
