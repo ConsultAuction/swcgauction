d4e3258262d463bdcde1daaa37903a7ab5795be4
Tobias Håkansson <tobias19995@gmail.com>
2020-12-15 15:30:26 +0100

    Classes and Interface added

---
+++ src/main/java/se/swcg/consultauction/client/repository/ClientRepository.java
@@ -0,0 +1,4 @@
+package se.swcg.consultauction.client.repository;
+
+public class ClientRepository {
+}
---
+++ src/main/java/se/swcg/consultauction/client/service/ClientService.java
@@ -0,0 +1,4 @@
+package se.swcg.consultauction.client.service;
+
+public interface ClientService {
+}
