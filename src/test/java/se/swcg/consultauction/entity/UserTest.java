ed0f42d110659864d785e3753374c3589ad5d016
RobinSandberg <38344081+RobinSandberg@users.noreply.github.com>
2020-12-16 15:42:58 +0100

    added test propertise and set H2 server to test scope in pom. Usertest in progress.

--- src/test/java/se/swcg/consultauction/entity/UserTest.java
+++ src/test/java/se/swcg/consultauction/entity/UserTest.java
@@ -1,6 +1,7 @@
 package se.swcg.consultauction.entity;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
@@ -13,8 +14,16 @@ class UserTest {
 
     @BeforeEach
     void setUp(){
-        //testobject = new User("Robin", "Sandberg", "Robin@mail.com", true, LocalDate.now().minusDays(), LocalDate.now())
+        testobject = new User( "id1" , "Robin", "Sandberg", "Robin@mail.com", true, LocalDate.now(), LocalDate.now(), true , "1234" , "user" , "0147852", "image1", 50, null);
     }
 
+    @Test
+    void getTest(){
+       User newUser = new User(testobject.getUserId(), testobject.getFirstName(), testobject.getLastName(), testobject.getEmail(),
+               testobject.isActive() , testobject.getDateOfSignUp(), testobject.getLastActive(), testobject.isAvailable(), testobject.getPassword(), testobject.getRole(),
+               testobject.getPhoneNumber(), testobject.getImage(), testobject.getMinPrice(), testobject.getQualifications());
+
+        assertEquals(newUser , testobject);
+    }
 
 }
