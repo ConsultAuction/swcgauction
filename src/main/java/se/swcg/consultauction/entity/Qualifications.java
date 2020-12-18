80e2a878d3a1b28faec1c2ab7f8a2b6800cb7f01
Adam <adamlundberg91@gmail.com>
2020-12-18 09:59:01 +0100

    remove Certificate.java

--- src/main/java/se/swcg/consultauction/entity/Qualifications.java
+++ src/main/java/se/swcg/consultauction/entity/Qualifications.java
@@ -20,23 +20,19 @@ public class Qualifications {
     private boolean backend;
 
     @OneToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.LAZY)
-    private List<Certificate> certificate;
-
-    @OneToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.LAZY)
     private List<Experience> experience;
 
     @OneToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.LAZY)
     private List<Languages> language;
 
-    public Qualifications(String qualificationsId, boolean frontend, boolean backend, List<Certificate> certificate, List<Experience> experience, List<Languages> language) {
-        this(frontend, backend, certificate, experience, language);
+    public Qualifications(String qualificationsId, boolean frontend, boolean backend, List<Experience> experience, List<Languages> language) {
+        this(frontend, backend, experience, language);
         this.qualificationsId = qualificationsId;
     }
 
-    public Qualifications(boolean frontend, boolean backend, List<Certificate> certificate, List<Experience> experience, List<Languages> language) {
+    public Qualifications(boolean frontend, boolean backend, List<Experience> experience, List<Languages> language) {
         this.frontend = frontend;
         this.backend = backend;
-        this.certificate = certificate;
         this.experience = experience;
         this.language = language;
     }
@@ -45,14 +41,6 @@ public class Qualifications {
     }
 
     // TODO add validation for all add/remove methods
-    public void addCertificate(Certificate certificateToAdd) {
-        certificate.add(certificateToAdd);
-    }
-
-    public void removeCertificate(Certificate certificateToRemove) {
-        certificate.remove(certificateToRemove);
-    }
-
     public void addExperience(Experience experienceToAdd) {
         experience.add(experienceToAdd);
     }
@@ -81,10 +69,6 @@ public class Qualifications {
         return backend;
     }
 
-    public List<Certificate> getCertificate() {
-        return certificate;
-    }
-
     public List<Experience> getExperience() {
         return experience;
     }
@@ -101,11 +85,7 @@ public class Qualifications {
         this.backend = backend;
     }
 
-   /* public void setCertificate(List<Certificate> certificate) {
-        this.certificate = certificate;
-    }
-
-    public void setExperience(List<Experience> experience) {
+    /*public void setExperience(List<Experience> experience) {
         this.experience = experience;
     }
 
@@ -121,23 +101,21 @@ public class Qualifications {
         return frontend == that.frontend &&
                 backend == that.backend &&
                 Objects.equals(qualificationsId, that.qualificationsId) &&
-                Objects.equals(certificate, that.certificate) &&
                 Objects.equals(experience, that.experience) &&
                 Objects.equals(language, that.language);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(qualificationsId, frontend, backend, certificate, experience, language);
+        return Objects.hash(qualificationsId, frontend, backend, experience, language);
     }
 
     @Override
     public String toString() {
         return "Qualifications{" +
-                "id='" + qualificationsId + '\'' +
+                "qualificationsId='" + qualificationsId + '\'' +
                 ", frontend=" + frontend +
                 ", backend=" + backend +
-                ", certificate=" + certificate +
                 ", experience=" + experience +
                 ", language=" + language +
                 '}';
