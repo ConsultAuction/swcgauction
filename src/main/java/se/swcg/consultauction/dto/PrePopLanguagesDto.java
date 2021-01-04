84bfac8bdf6a2529c7a5790eed02cddb15adbe4a
Adam <adamlundberg91@gmail.com>
2021-01-04 11:58:18 +0100

    Added PrePopLanguagesDto

---
+++ src/main/java/se/swcg/consultauction/dto/PrePopLanguagesDto.java
@@ -0,0 +1,51 @@
+package se.swcg.consultauction.dto;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+
+public class PrePopLanguagesDto {
+
+    private String languagesId;
+
+    @NotBlank
+    private String language;
+
+    public PrePopLanguagesDto(String languagesId, @NotBlank String language) {
+        this.languagesId = languagesId;
+        this.language = language;
+    }
+
+    public String getLanguagesId() {
+        return languagesId;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PrePopLanguagesDto that = (PrePopLanguagesDto) o;
+        return Objects.equals(languagesId, that.languagesId) &&
+                Objects.equals(language, that.language);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(languagesId, language);
+    }
+
+    @Override
+    public String toString() {
+        return "PrePopLanguagesDto{" +
+                "languagesId='" + languagesId + '\'' +
+                ", language='" + language + '\'' +
+                '}';
+    }
+}
