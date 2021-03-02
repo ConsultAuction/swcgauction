ab17751bd407f593a95c9232925e58099282f7f0
Jon0945 <john.gullne@gmail.com>
2021-03-02 13:42:53 +0100

    Login functionality

--- client/src/component/auth/Login.js
+++ client/src/component/auth/Login.js
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
+
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+      });
+
+      const { email, password } = user;
+
+    const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+    
     return (
         <div className="container" style={{maxWidth: '500px'}}>
         <form>
             <h1>Account Login</h1>
             <div className="form-group">
                 <label htmlFor="Email">Username / Email address</label>
-                <input type="email" className="form-control" name="email"/>
+                <input 
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    value={email}
+                    onChange={onChange}
+                    required
+                />
             </div>
             <div className="form-group">
                 <label htmlFor="Password">Password</label>
-                <input type="password" className="form-control" name="password" />
+                <input 
+                    type="password" 
+                    className="form-control" 
+                    name="password"
+                    value={password}
+                    onChange={onChange}
+                    required />
             </div>
+            </form>
+            <div className="text-left mb-2">Forgot Password?</div>
             <button type="submit" className="btn btn-primary">Login</button>
-        </form>
+        
+        
         </div>
         
     )
